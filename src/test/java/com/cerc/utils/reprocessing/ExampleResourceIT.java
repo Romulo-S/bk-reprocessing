2f2bf7bfc0f417cfb8d2c9d99222da4fbff5c1c9
Romulo <tp.romulo.domingos@totvspartners.com.br>
2022-06-04 14:17:38 -0300

    Creates project structure

---
+++ src/test/java/com/cerc/utils/reprocessing/ExampleResourceIT.java
@@ -0,0 +1,9 @@
+package com.cerc.utils.reprocessing;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class ExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
